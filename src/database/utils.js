f9695aa8c03a60fb43c880ac103f5d036bb3588e
fuentestramita <151641594+fuentestramita@users.noreply.github.com>
2023-12-11 18:18:08 -0300

    Borrado

--- src/database/utils.js
+++
@@ -1,6 +0,0 @@
-import data from "../src/database/database.json" assert { type: "json" };
-
-const Usuarios = data.Usuarios;
-console.log("utils: " + Usuarios);
-
-module.exports = { Usuarios };
--- src/routes/Routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const ValidateController = require("../controllers/ValidateController");
-const LoginController = require("../controllers/LoginController");
-const router = express.Router();
-
-router.post("/validate", ValidateController.doValidate);
-router.post("/login", LoginController.doLogin);
-
-module.exports = router;
