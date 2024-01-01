bc3f4b490af3b2b63ad9fc402c6f8bc0c5d0c6f6
Christopher Granger <chris_ciber.2000@hotmail.cl>
2023-12-31 23:28:11 -0300

    Modified DB Driver from nodev8 to mssql

--- src/database/db.js
+++ src/database/db.js
@@ -1,79 +1,87 @@
 require("dotenv").config();
-const sql = require("msnodesqlv8");
+const sql = require("mssql");
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
 const DB_HOST = process.env.DB_HOST;
 const DB_NAME = process.env.DB_NAME;
-const connectionString = `server=${DB_HOST};Database=${DB_NAME};UID=${DB_USER};PWD=${DB_PASS};Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server}`;
+const sqlConfig = {
+  user: `${DB_USER}`,
+  password: `${DB_PASS}`,
+  database: `${DB_NAME}`,
+  server: `${DB_HOST}`,
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000,
+  },
+  authentication: {
+    type: "default",
+  },
+  options: {
+    trustServerCertificate: true, // change to true for local dev / self-signed certs
+  },
+};
+
+let poolRequest;
+
+// returns a promise which resolves to the current connection pool
+function getConnection() {
+  if (!poolRequest) {
+    const pool = new sql.ConnectionPool(sqlConfig);
+    poolRequest = pool.connect();
+  }
+  return poolRequest;
+}
 
 const dbLogin = async (idLogin, passLogin) => {
   const foundUser = await dbVerify(idLogin);
   if (foundUser.length == 0) {
     return null;
   }
-  return new Promise((resolve, reject) => {
-    try {
-      sql.query(
-        connectionString,
-        `exec SEL_Login '${idLogin}', '${passLogin}'`,
-        (err, rows) => {
-          if (err != null) console.log(err);
-          resolve(rows);
-        }
-      );
-    } catch (error) {
-      return reject(error);
-    }
-  });
+  try {
+    let pool = await getConnection();
+    let result1 = await pool
+      .request()
+      .query(`exec SEL_Login '${idLogin}', '${passLogin}'`);
+    return result1.recordset;
+  } catch (err) {
+    throw err;
+  }
 };
 
 const dbVerify = async (idLogin) => {
-  return new Promise((resolve, reject) => {
-    try {
-      sql.query(
-        connectionString,
-        `Select * from dbo.Usuarios where RUTUsuario='${idLogin}'`,
-        (err, rows) => {
-          if (err != null) console.log(err);
-          resolve(rows);
-        }
-      );
-    } catch (error) {
-      return reject(error);
-    }
-  });
+  try {
+    let pool = await getConnection();
+    let result1 = await pool
+      .request()
+      .query(`Select * from dbo.Usuarios where RUTUsuario='${idLogin}'`);
+    return result1.recordset;
+  } catch (err) {
+    throw err;
+  }
 };
 
 const dbSetCodigo = async (id, codigo) => {
-  return new Promise((resolve, reject) => {
-    try {
-      sql.query(
-        connectionString,
-        `exec INS_CodigoLogin ${id}, '${codigo}'`,
-        (err, rows) => {
-          if (err != null) console.log(err);
-          resolve(rows);
-        }
-      );
-    } catch (error) {
-      return reject(error);
-    }
-  });
+  try {
+    let pool = await getConnection();
+    let result1 = await pool
+      .request()
+      .query(`exec INS_CodigoLogin ${id}, '${codigo}'`);
+
+    return result1.recordset;
+  } catch (err) {
+    throw err;
+  }
 };
 const dbGetCodigo = async (id, codigo) => {
-  return new Promise((resolve, reject) => {
-    try {
-      sql.query(
-        connectionString,
-        `exec SEL_ValidaCodigoUsuario '${id}', '${codigo}'`,
-        (err, rows) => {
-          if (err != null) console.log(err);
-          resolve(rows);
-        }
-      );
-    } catch (error) {
-      return reject(error);
-    }
-  });
+  try {
+    let pool = await getConnection();
+    let result1 = await pool
+      .request()
+      .query(`exec SEL_ValidaCodigoUsuario '${id}', '${codigo}'`);
+    return result1.recordset;
+  } catch (err) {
+    throw err;
+  }
 };
 module.exports = { dbLogin: dbLogin, dbSetCodigo, dbGetCodigo };
