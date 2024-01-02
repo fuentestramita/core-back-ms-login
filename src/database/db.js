e09e9079320dbadebe87eae5e1d0a49511212402
fuentestramita <151641594+fuentestramita@users.noreply.github.com>
2024-01-02 11:37:25 -0300

    Update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -16,10 +16,9 @@ const sqlConfig = {
     idleTimeoutMillis: 30000,
   },
   options: {
-    encrypt: true, //for azure
-    trustServerCertificate: false, // change to true for local dev / self-signed certs
-    truestedConnection: false,
-    hostNameInCertificate=*.database.windows.net
+    encrypt: false, //for azure
+    trustServerCertificate: true, // change to true for local dev / self-signed certs
+    truestedConnection: true
   },
 };
 
